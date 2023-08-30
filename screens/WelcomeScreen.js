b3679e29db0dc580f43a96a5cbcbdcea1b0e5b35
Umar <omarsabiu679@gmail.com>
2023-08-30 15:02:56 +0100

    Aligned aligned both login and Register button to center

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -38,8 +38,9 @@ export default WelcomeScreen;
 
 const styles = StyleSheet.create({
   buttonContainer: {
-    position: "absolute",
     width: 350,
+    margin: 20,
+    alignItems: "center",
   },
   headerContainer: {
     top: 50,
