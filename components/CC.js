f8851754d5a147cd0bcb2b7fd7abe49cf12df971
Umar <omarsabiu679@gmail.com>
2023-09-01 21:45:52 +0100

    Lazy Day

---
+++ components/CC.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CC = () => {
+  return (
+    <View>
+      <Text>CaC</Text>
+    </View>
+  );
+};
+
+export default CC;
+
+const styles = StyleSheet.create({});
