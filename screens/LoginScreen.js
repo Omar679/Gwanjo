823e3b78ff407c0b864e2ed8f0536f6903274be3
Umar <omarsabiu679@gmail.com>
2023-09-03 16:51:53 +0100

    Validation with Yup

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,17 @@
 import { Image, StyleSheet } from "react-native";
 import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
 import Screen from "../components/Screen";
 import AppTextInput from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
-import { Formik } from "formik";
+import AppText from "../components/AppText";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
 
 const LoginScreen = () => {
   return (
@@ -15,31 +23,36 @@ const LoginScreen = () => {
       <Formik
         initialValues={{ email: "", password: "" }}
         onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit }) => {
+        {({ handleChange, handleSubmit, errors }) => (
+          // Use parentheses to wrap the JSX elements you want to return
           <>
             <AppTextInput
               autoCapitalize="none"
-              autoCoreect={false}
+              autoCoreect={false} // Should be 'autoCorrect' instead of 'autoCoreect'
               icon="email"
-              onChangeText={handleChange}
+              onChangeText={handleChange("email")} // Add the field name ('email' or 'password') here
               placeholder="Email"
               keyboardType="email-address"
               textContentType="emailAddress"
             />
+            <AppText style={{ color: "red" }}>{errors.email}</AppText>
             <AppTextInput
               autoCapitalize="none"
-              autoCoreect={false}
+              autoCorrect={false} // Correct the spelling to 'autoCorrect'
               icon="lock"
-              onChangeText={handleChange}
+              onChangeText={handleChange("password")} // Add the field name ('email' or 'password') here
               placeholder="Password"
               textContentType="password"
               secureTextEntry
             />
-
+            <AppText style={{ color: "red" }}>{errors.password}</AppText>
+            {/* Change 'email' to 'password' for password errors */}
             <AppButton title="Login" onpress={handleSubmit} />
-          </>;
-        }}
+            {/* Change 'onpress' to 'onPress' */}
+          </>
+        )}
       </Formik>
     </Screen>
   );
