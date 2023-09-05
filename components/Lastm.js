f93d30aac14717a49e0d93eb776a8ea26ab9243c
Umar <omarsabiu679@gmail.com>
2023-09-05 23:30:28 +0100

    Last minuet Commit

---
+++ components/Lastm.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Lastm = () => {
+  return (
+    <View>
+      <Text>Lastm</Text>
+    </View>
+  );
+};
+
+export default Lastm;
+
+const styles = StyleSheet.create({});
