5fd63501deadaa18a7d16b07f4fc62ef31c55104
Umar <omarsabiu679@gmail.com>
2023-09-07 09:17:39 +0100

    Improving Listing Edit Screen

--- App.js
+++ App.js
@@ -1,42 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-import { Image, StyleSheet } from "react-native";
-import Screen from "./components/Screen";
-import ImageInputList from "./components/ImageInputList";
-import { ScrollView } from "react-native";
-import { View } from "react-native";
+import ListingEditScreen from "./screens/ListingEditScreen";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
-  const scrollView = useRef();
-
-  return (
-    <View>
-      <ScrollView
-        horizontal
-        ref={scrollView}
-        onContentSizeChange={() => scrollView.current.scrollToEnd()}
-      >
-        <Screen style={styles.container}>
-          <ImageInputList
-            imageUris={imageUris}
-            onAddImage={handleAdd}
-            onRemoveImage={handleRemove}
-          />
-        </Screen>
-      </ScrollView>
-    </View>
-  );
+  return <ListingEditScreen />;
 }
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 30,
-  },
-});
