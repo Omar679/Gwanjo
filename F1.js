6f21366119fee28a56b2475f6299d2065510e017
Umar <omarsabiu679@gmail.com>
2023-09-09 20:20:41 +0100

    Full The day

---
+++ F1.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const F1 = () => {
+  return (
+    <View>
+      <Text>F1</Text>
+    </View>
+  );
+};
+
+export default F1;
+
+const styles = StyleSheet.create({});
---
+++ F2.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const F2 = () => {
+  return (
+    <View>
+      <Text>F2</Text>
+    </View>
+  );
+};
+
+export default F2;
+
+const styles = StyleSheet.create({});
