dc11da4f565eeb5679c02a6bdf5dbe7091d024bf
Umar <omarsabiu679@gmail.com>
2023-09-09 20:21:18 +0100

    Full The day

---
+++ F3.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const F3 = () => {
+  return (
+    <View>
+      <Text>F3</Text>
+    </View>
+  );
+};
+
+export default F3;
+
+const styles = StyleSheet.create({});
